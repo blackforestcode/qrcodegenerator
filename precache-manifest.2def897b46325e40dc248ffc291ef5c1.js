dd613f7b2219e7849a8bb23c7454898940ab06ff
Nils Domin <nils@blackforestcode.com>
2019-07-20 15:06:05 +0200

    Updates

---
+++ precache-manifest.2def897b46325e40dc248ffc291ef5c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464a5097eb798a89dc53a9770324fcfd",
+    "url": "/qrcodegenerator/index.html"
+  },
+  {
+    "revision": "1f24a9b3b4fb35130f69",
+    "url": "/qrcodegenerator/static/css/main.d9507cc5.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/qrcodegenerator/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "1f24a9b3b4fb35130f69",
+    "url": "/qrcodegenerator/static/js/main.3cb1f0f7.chunk.js"
+  },
+  {
+    "revision": "eff5d862ea6e2995bce0",
+    "url": "/qrcodegenerator/static/js/runtime~main.c78b961e.js"
+  }
+]);
