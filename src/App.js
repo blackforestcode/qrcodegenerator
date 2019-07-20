f1a8777dc66f4a33a01c3ac44a7ff72743fddc00
Nils Domin <nils@blackforestcode.com>
2019-07-20 14:56:28 +0200

    Added some functionality

--- src/App.js
+++ src/App.js
@@ -2,41 +2,131 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [errorText, setErrorText] = useState('');
   const [url, setUrl] = useState('');
   const [qr, setQr] = useState('');
+  const [spinner, setSpinner] = useState(false);
 
   const onChange = e => setUrl(e.target.value);
 
   const onSubmit = e => {
     e.preventDefault();
-    if (url === '') {
-      alert('Please enter an url.');
+    if (url === '' || !url.includes('http') || !url.includes('https')) {
+      setErrorText('Please enter a valid URL with http:// or https://');
     } else {
-      setQr(
-        `https://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`
-      );
+      setSpinner(true);
+
+      setTimeout(() => {
+        setQr(
+          `https://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`
+        );
+        setSpinner(false);
+      }, 500);
     }
+  };
+
+  const resetForm = () => {
+    setUrl('');
+    setQr('');
   };
 
   return (
     <div className="App" style={{ padding: '5%' }}>
-      <h1>QR Code Generator</h1>
-      <form className="form" onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="url"
-          placeholder="Enter a URL..."
-          value={url}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value="Create QR-Code"
-          className="btn btn-dark btn-block"
-        />
-      </form>
-      <div style={{ margin: '5%' }}>
-        <img src={qr} alt="QR Code" />
+      <div className="container" style={{ height: '100vh' }}>
+        <div className="row">
+          <div className="column">
+            <h1>QR Code Generator</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <form
+              className="form"
+              onSubmit={onSubmit}
+              style={{ marginTop: '3%' }}
+            >
+              <input
+                type="url"
+                name="text"
+                placeholder="Enter your URL with http://... or https://..."
+                value={url}
+                onChange={onChange}
+                onKeyDown={() => setErrorText('')}
+              />
+              {qr.length > 0 ? (
+                <span
+                  className="float-right"
+                  onClick={resetForm}
+                  style={{ cursor: 'pointer' }}
+                >
+                  Delete <span style={{ color: '#ff4d4d' }}>X</span>
+                </span>
+              ) : null}
+              {errorText !== '' ? (
+                <p style={{ color: '#ff4d4d' }}>{errorText}</p>
+              ) : null}
+              <input
+                className="button-primary"
+                type="submit"
+                value="Create QR-Code"
+              />
+            </form>
+          </div>
+        </div>
+
+        {spinner ? (
+          <div className="row">
+            <div className="column column-50 column-offset-33">
+              <div className="spinner" />
+            </div>
+          </div>
+        ) : null}
+
+        <div className="row">
+          <div className="column column-50 column-offset-33">
+            {qr.length > 0 ? (
+              <>
+                <img src={qr} alt={`QR Code of ${url}`} />
+                <figcaption>{url}</figcaption>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="column">
+            <p>
+              Stack:
+              <br />
+              <span className="badge">React.js</span>{' '}
+              <span className="badge">Milligram CSS</span>{' '}
+              <span className="badge">
+                QR Code API by{' '}
+                <a
+                  href="http://goqr.me/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  http://goqr.me/
+                </a>
+              </span>
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="column">
+            made with &hearts; by{' '}
+            <a
+              href="https://blackforestcode.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BLACK FOREST CODE
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
